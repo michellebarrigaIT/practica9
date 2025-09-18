adcca98c2b7350c7536504f410217b16bad59a91
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-18 12:55:14 -0400

    feat: add home

---
+++ src/components/BookCard.tsx
@@ -0,0 +1,20 @@
+import type { FC } from "react";
+import type { Book } from "../types/Book";
+import { Link } from "react-router-dom";
+
+
+type BookCardProps = {
+    book: Book;
+}
+
+const BookCard: FC<BookCardProps> = ({ book }) => {
+  return (
+    <div className="book-card">
+      <h3>{book.title}</h3>
+      <p><strong>Author:</strong> {book.author}</p>
+      <Link to={`/books/${book.id}`}>View Details</Link>
+    </div>
+  );
+};
+
+export default BookCard;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import "./Home.scss"
 
 function Home() {
   return (
-    <div className='home-container'>
-        <h1>Welcome to MyBooks App</h1>
-        <button>Check Books</button>
+    <div className="home">
+      <h1>Welcome to the Book Management App </h1>
+      <Link to="/books">Go to Books</Link>
     </div>
   )
 }
