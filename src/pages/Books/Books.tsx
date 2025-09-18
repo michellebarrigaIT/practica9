855c65cfd8b4b60d39d55b61d3ec9d779b601e88
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-18 13:01:30 -0400

    feat: display books

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -1,7 +1,16 @@
+import BookCard from "../../components/BookCard"
+import { books } from "../../data/books"
 
 function Books() {
   return (
-    <div>Books</div>
+    <div className="books">
+      <h2>Book List</h2>
+      <div className="books-list">
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </div>
   )
 }
 
