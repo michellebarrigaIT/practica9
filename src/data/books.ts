1c86a4d31e5cb576ccb16472c5570c9ee93107f2
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-18 12:51:24 -0400

    feat: add types

---
+++ src/data/books.ts
@@ -0,0 +1,22 @@
+import type { Book } from "../types/Book";
+
+export const books: Book[] = [
+  {
+    id: "1",
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    description: "A handbook of agile software craftsmanship."
+  },
+  {
+    id: "2",
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt, David Thomas",
+    description: "Journey to mastery in software development."
+  },
+  {
+    id: "3",
+    title: "Refactoring",
+    author: "Martin Fowler",
+    description: "Improving the design of existing code."
+  }
+];
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.scss"
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <h1>Welcome to MyBooks App</h1>
+        <button>Check Books</button>
+    </div>
+  )
+}
+
+export default Home;
